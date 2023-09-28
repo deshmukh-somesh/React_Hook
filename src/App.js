f5432d5512cbc79ad5daa87c14a5d09269c9b51f
Somesh Deshmukh <somesh.deshmukh@blenheimchalcot.com>
2023-09-28 11:33:02 +0530

    1st class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-
+import './components/ClassCounter'
+import ClassCounter from './components/ClassCounter';
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      
+      <ClassCounter/>
     </div>
   );
 }
